bba8efd3684ebc0906c89aec5d45ced9ce593595
r4st4m4n <r4st4m4n@r4st4m4ns-MacBook-Pro.local>
2022-07-14 20:00:33 -0500

    adding flags

--- cmd/territoriumsyncv2.go
+++ cmd/territoriumsyncv2.go
@@ -5,6 +5,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var path string
+var downloadtype string
+var bucketname string
 var territoriumsync = &cobra.Command{
 	Use:   "territoriumsyncv2",
 	Short: "test",
@@ -16,5 +19,8 @@ var territoriumsync = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(territoriumsync)
+	territoriumsync.Flags().StringVarP(&path, "pathfile", "p", "", "Define the pathfile of csv")
+	territoriumsync.Flags().StringVarP(&downloadtype, "downloadtype", "d", "local", "define the download type local or uploadtoaws")
+	territoriumsync.Flags().StringVarP(&bucketname, "bucketname", "b", "", "define the bucket to upload the data")
 
 }
